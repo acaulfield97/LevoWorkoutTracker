a0c032d93fa1556fe0671ece53eea6a8235b93ac
acaulfield97 <annacaulfield04@hotmail.com>
2025-12-09 22:05:11 +0000

    Comments for clarity

--- src/main/java/com/levo/levofitnessapp/controller/ExerciseController.java
+++ src/main/java/com/levo/levofitnessapp/controller/ExerciseController.java
@@ -14,8 +14,12 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Collections;
 
+// got rid of unnecessary long routes (e.g. /exercise/select-exercise)
+// now the /exercise route is configured by adding parameters each time a selection is made
+// e.g. when a category is selected -> /exercise?categoryId=1
+// and when an exercise is selected -> /exercise?categoryId=1&exerciseId=2
 @Controller
-@RequestMapping("/exercise")
+@RequestMapping("/exercise") // the base route
 public class ExerciseController {
 
     private final ExerciseRepository exerciseRepository;
@@ -53,12 +57,17 @@ public class ExerciseController {
         return modelAndView;
     }
 
+    // doesn't do anything yet
+    // for a user to add a new exercise
     @PostMapping("/add-exercise")
     public RedirectView addExercise(Exercise exercise){
         exerciseRepository.save(exercise);
         return new RedirectView("/exercise");
     }
 
+    // this then routes to new sets page: /exercise/exercise-set?categoryId=1&exerciseId=2
+    // possibly needs refactored for clarity???
+    // e.g. instead just route to /set ? idk
     @GetMapping("/exercise-set")
     public ModelAndView showExerciseSet(@RequestParam Long exerciseId,
                                         @ModelAttribute("currentUserId") Long userId) {
