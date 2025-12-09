4cf54a12af6631c5e5526c6b45ceab3389f80b86
Patrick Bateson <pbateson4@icloud.com>
2025-12-09 14:22:30 +0000

    added repo, controller, template and model for adding an exercise

---
+++ src/main/java/com/levo/levofitnessapp/repository/ExerciseRepository.java
@@ -0,0 +1,7 @@
+package com.levo.levofitnessapp.repository;
+
+import com.levo.levofitnessapp.model.Exercise;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExerciseRepository extends CrudRepository<Exercise, Long>{
+}
