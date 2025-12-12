41ed2c246f7ccb8511ab696de53dbaaaeb952cc4
Lauren Miller <lmiller08@qub.ac.uk>
2025-12-12 10:33:41 +0000

    Adding function which shows current workout

--- src/main/java/com/levo/levofitnessapp/repository/WorkoutExerciseRepository.java
+++ src/main/java/com/levo/levofitnessapp/repository/WorkoutExerciseRepository.java
@@ -4,9 +4,14 @@ import com.levo.levofitnessapp.model.Exercise;
 import com.levo.levofitnessapp.model.WorkoutExercise;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WorkoutExerciseRepository extends CrudRepository<WorkoutExercise, Long> {
 
     Optional<WorkoutExercise> findByExerciseId(Exercise exerciseId);
+
+    // find all exercise_ids for a given workout_id
+    List<WorkoutExercise> findByWorkoutId(Long workoutId);
+
 }
